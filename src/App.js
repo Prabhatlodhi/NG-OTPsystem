4e08dd91ae59265ddebb32c0491e9c13cc67b67d
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-04-04 16:21:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState("");
+
+  function handleChange(event) {
+    const input = event.target.value;
+    if (!isNaN(input) && input.length <= 1) {
+      setNumber(input);
+    } else {
+      setNumber("");
+    }
+  }
+
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="child_div" >
+          <h3>Enter verification code</h3>
+          <div>
+     
+        <input
+          
+          type="tel"
+          value={number}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+
+ 
+     
+    </div>
+          <div>
+            <button>Clear</button>
+            <button>Get OTP</button>
+          </div>
+       </div>
     </div>
   );
 }
